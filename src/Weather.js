985000f6abc283b4e994569f87a97806609e64b3
ewalker24 <114313334+ewalker24@users.noreply.github.com>
2023-02-11 14:53:39 -0800

    Update Weather.js
    import axios and made api call

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,12 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-  return <div></div>;
+  let apiKey = "094780c710fa4efd669f0df8c3991927";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+
+  function showWeather(response) {
+    alert(response.data.main.temp);
+  }
+  return <div>Hello from Weather</div>;
 }
