41e75d7c379445773bdbb532c96b477c770b3404
ewalker24 <114313334+ewalker24@users.noreply.github.com>
2023-02-26 16:54:05 -0800

    New component credits

---
+++ src/Credits.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Credits() {
+  return (
+    <div className="credits mt-3">
+      <span>
+        This project was coded by{" "}
+        <a href="https://stately-blini-d72870.netlify.app/">Emily Walker</a> and
+        is{" "}
+        <a href="https://github.com/ewalker24/my_react">
+          open-sourced on GitHub
+        </a>
+      </span>{" "}
+    </div>
+  );
+}
